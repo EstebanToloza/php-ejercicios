720d0d6c33149723d2cbf437100af5bbae51d375
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-10-28 23:45:01 -0300

    hola mundo en php

---
+++ 01-hola-mundo.php
@@ -0,0 +1,15 @@
+<?php include 'includes/header.php';
+
+// <?php
+// echo "Hola mundo <br>";
+
+// print "Hola mundo con print <br>";
+
+// var_dump("Hola mundo con var_dump");
+
+
+
+
+
+
+include 'includes/footer.php';
