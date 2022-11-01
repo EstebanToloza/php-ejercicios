20b1b767184fa9d7790e9ec7134e1c6d888ce209
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-10-31 22:19:15 -0300

    variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,14 @@
 <?php include 'includes/header.php';
 
+$nombre = "Esteban"; //para el naming aplican las mismas reglas que para js, sólo que puede comenzar y terminar con guion bajo
+$nombre = "Esteban reasignado";
+echo $nombre;
 
+define("constante", "<br>Éste es el valor"); //Define es una "propiedad" nueva
+echo constante;
+
+const constante2 = "<br>Hola constante 2"; //otra manera, no tan utilizada
+echo constante2;
 
 
 include 'includes/footer.php';
