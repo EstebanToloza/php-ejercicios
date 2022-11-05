fcf9742b29d7bdd32b4f9bfcd56cc1dd9291f42a
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-11-04 23:24:30 -0300

    Condicionales, loops, foreach, funciones

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,6 +1,51 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = false;
 
+if ($autenticado && $admin) {
+    echo "autenticado";
+} else {
+    echo "no autenticado";
+}
+
+//ifs anidados
+$cliente = [
+    "nombre" => "Juan",
+    "saldo" => 10,
+    "info" => [
+        "tipo" => "premium"
+    ]
+];
+
+if (!empty($cliente)) {
+    echo "El cliente existe";
+
+    if ($cliente["saldo"] > 0) {
+        echo "el cliente tiene saldo";
+    } else {
+        echo "No tiene saldo";
+    }
+}
+
+if ($cliente["saldo"]) {
+    echo "El cliente tiene saldo";
+} else if ($cliente["info"]["tipo"] === "premium") {
+    echo "es premium";
+} else {
+    echo "no tiene un mango";
+}
+
+$tecnologia = "PHP";
+
+switch ($tecnologia) {
+    case "PHP":
+        echo "PHP Es lo más";
+        break;
+    default:
+        echo "Guien soo";
+        break;
+}
 
 
 
--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,23 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function sumar(int $numero1 = 0, int $numero2)
+{
+    echo $numero1 + $numero2;
+}
 
+sumar(10, 10);
+
+echo "<br>";
+
+function sumar2(int $numero1 = 0, array $numero2)
+{
+    echo $numero1 + $numero2;
+}
+
+sumar2(10, []);
 
 
 include 'includes/footer.php';
